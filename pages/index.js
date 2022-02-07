893f6635ab6389cc22eb00072cb4ac8652f141a9
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-02-07 17:06:53 -0600

    Add function to calculate winner

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,46 @@
 import { useEffect, useState } from "react"
 import { Block } from '@components/block';
 const HomePage = () => {
+
+    const [winner, setWinner] = useState(null);
     const [xIsNext, setXIsNext] = useState(true);
     const [board,setBoard] = useState(Array(9).fill(null));
 
     const onClick = (_index) => {
-        if(board[_index] ==  null){
+        
+        if(board[_index] == null && winner == null){
             const _board = [...board];
             _board[_index] = xIsNext ? 'X' : 'O';
             setBoard(_board);
             setXIsNext(!xIsNext);
+            setWinner(calcWinner(_board));
         }
     }
 
+    const calcWinner = (_board) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+         for (let counter = 0; counter < lines.length; counter++) {
+            const [a, b, c] = lines[counter];
+            if (_board[a] && _board[a] === _board[b] && _board[a] === _board[c]) {
+                return _board[a]
+            }
+        }
+
+        return null;
+    };
+
     return  <div className="container">
+                The Winner is player { winner }
                 <div className="row">
                     {board.map((value, index) => {
                         return (
