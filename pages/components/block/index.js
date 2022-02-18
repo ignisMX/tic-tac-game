24c1928e0a8258eedb46b66b3d12e2e77c851542
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-02-17 23:31:22 -0600

    add size to grid and center the symbols

--- pages/components/block/index.js
+++ pages/components/block/index.js
@@ -1,5 +1,5 @@
 export const Block = (props) =>{
     const { index, onClick, value } = props;
 
-    return <div onClick={()=>{ onClick(index) }} className="border">{value} element</div>
+    return <div onClick={()=>{ onClick(index) }} className="border square">{value}</div>
 }
