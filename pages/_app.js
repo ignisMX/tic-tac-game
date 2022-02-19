5ade91140e4970bc8484b64f93b0ca9dca516da8
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-02-18 23:04:23 -0600

    Add icon fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './styles.css';
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
--- pages/components/block/index.js
+++ pages/components/block/index.js
@@ -1,5 +1,17 @@
 export const Block = (props) =>{
+
     const { index, onClick, value } = props;
 
-    return <div onClick={()=>{ onClick(index) }} className="border square">{value}</div>
+    const Symbol = () => {
+        let tag = <i className=""></i>
+        if(value != null && value != undefined){
+            tag = <i className={`${value == 'X' ? 'bi-x symbolX' : 'bi-circle symbolO'}`}></i>
+        }
+
+        return tag;
+    }
+
+    return <div onClick={()=>{ onClick(index) }} className="border square">
+                <Symbol />
+            </div>
 }
